45e214e8e28b413387d4eaa77018b4b0091a8f31
Vincent.Li <vincentlee99@126.com>
2020-08-02 23:38:12 +0800

    upgrade springboot version to safe edition

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 ## springcloud-app-parent
 ### 工程说明
-* springcloud-app-parent是一个练习SpringCloud的demo工程
+* springcloud-app-parent是一个SpringCloud的练习实践项目
 * Spring Cloud版本号是Greenwich.SR3
-* Spring Boot的版本号是2.1.10.RELEASE
+* Spring Boot的版本号是2.1.15.RELEASE
 ### 模块说明
-* springcloud-app-eureka-server  eureka做服务注册与发现中心
-* springcloud-app-eureka-client  注册服务（可供消费）
-* springcloud-app-config-server  分布式服务配置中心-配置服务提供者
-* springcloud-app-config-client  分布式服务配置中心-配置服务消费者
-* springcloud-app-ribbon-service 服务消费者（rest+ribbon）(负载均衡)
-* springcloud-app-common 公共模块
-* springcloud-app-gateway 网关使用demo （使用nacos做注册发现）
-* springcloud-app-api 网关代理api服务 （使用nacos做注册发现）
+1. springcloud-app-eureka-server  eureka做服务注册与发现中心
+2. springcloud-app-eureka-client  注册服务（可供消费）
+3. springcloud-app-config-server  分布式服务配置中心-配置服务提供者
+4. springcloud-app-config-client  分布式服务配置中心-配置服务消费者
+5. springcloud-app-ribbon-service 服务消费者（rest+ribbon）(负载均衡)
+6. springcloud-app-common 公共模块
+7. springcloud-app-gateway 网关使用demo （使用nacos做注册发现）
+8. springcloud-app-api 网关代理api服务 （使用nacos做注册发现）
+9. springcloud-app-kafka kafka消息生产消费的实践 
+* 9.1 springcloud-app-kafka-producer kafka消息生产
+* 9.2 springcloud-app-kafka-consumer kafka消息消费
---
+++ springcloud-app-kafka/springcloud-app-kafka-producer/src/main/java/com/licq/app/kafka/producer/service/KafkaProducer.java
@@ -0,0 +1,44 @@
+package com.licq.app.kafka.producer.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.licq.app.kafka.producer.constants.KafkaTopicConstant;
+import com.licq.app.kafka.producer.model.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * kafka消息生产
+ */
+@Component
+@Slf4j
+public class KafkaProducer {
+
+    @Resource
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private Gson gson = new GsonBuilder().create();
+
+    AtomicLong atomicLong = new AtomicLong(0L);
+
+    public void send() {
+        String msg = "中国崛起:"+atomicLong.incrementAndGet();
+        send(msg);
+    }
+
+
+    public void send(final String msg) {
+        Message message = new Message();
+        message.setId(System.currentTimeMillis());
+        message.setMsg(msg);
+        message.setSendTime(new Date());
+        log.info("+++++++++++++++++++++send  message = {}", gson.toJson(message));
+        kafkaTemplate.send(KafkaTopicConstant.testTopicName, message.getId().toString() , gson.toJson(message));
+    }
+}
