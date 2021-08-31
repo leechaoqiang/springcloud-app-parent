cfcb4891eb4b548f181bf912df9b341c18cc37a0
Vincent.Li <vincentlee99@126.com>
2021-08-31 17:07:39 +0800

    improve version

--- springcloud-app-apollo/springcloud-app-apollo-redis/src/main/java/com/vincent/app/apollo/redis/controller/PingController.java
+++ springcloud-app-apollo/springcloud-app-apollo-redis/src/main/java/com/vincent/app/apollo/redis/controller/PingController.java
@@ -3,7 +3,6 @@ package com.vincent.app.apollo.redis.controller;
 import com.google.common.collect.ImmutableMap;
 import com.vincent.app.apollo.redis.service.RedisService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
